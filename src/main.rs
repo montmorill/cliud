ec7a826f9668f7a96af2b988fd2966c0ce1ff67d
montmorill <montmorillonite@qq.com>
2024-07-13 10:37:16 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
@@ -8,9 +8,25 @@ fn main() {
 
     for mut stream in listener.incoming().map(Result::unwrap) {
         println!("accepted new connection");
-        let mut buf = String::new();
-        let _ = stream.read_to_string(&mut buf).unwrap();
-        println!("{buf}");
-        let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+
+        let mut request = String::new();
+        stream.read_to_string(&mut request).unwrap();
+        let mut splited = request.split("\r\n").into_iter();
+        let mut line = splited.next().unwrap().split(" ").into_iter();
+        let method = line.next().unwrap();
+        let target = line.next().unwrap();
+        let version = line.next().unwrap();
+
+        assert_eq!(version, "HTTP/1.1");
+        match method {
+            "GET" => {
+                let responce = match target {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+                stream.write(responce.as_bytes()).unwrap();
+            }
+            _ => unimplemented!(),
+        }
     }
 }
