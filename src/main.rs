6c2394bfe4e0a36bb79427ea2404b5d751806ab2
montmorill <montmorillonite@qq.com>
2024-07-14 08:09:48 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use http_server_starter_rust::request::{Request, Response, Result};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::{env, fs};
 
 const PROTOCOL: &str = "HTTP/1.1";
 
@@ -18,11 +19,21 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
                     .header("Content-Type", "text/plain")
                     .header("Content-Length", user_agent.len().to_string())
                     .body(user_agent)
-            } else if let Some(str) = target.strip_prefix("/echo/") {
+            } else if let Some(content) = target.strip_prefix("/echo/") {
                 Response::new(PROTOCOL, 200.to_string(), "OK")
                     .header("Content-Type", "text/plain")
-                    .header("Content-Length", str.len().to_string())
-                    .body(str)
+                    .header("Content-Length", content.len().to_string())
+                    .body(content)
+            } else if let Some(filename) = target.strip_prefix("/files/") {
+                let path = format!("{}/{}", env::args().nth(2).unwrap(), filename);
+                if let Ok(content) = fs::read(path) {
+                    Response::new(PROTOCOL, 200.to_string(), "OK")
+                        .header("Content-Type", "application/octet-stream")
+                        .header("Content-Length", content.len().to_string())
+                        .body(dbg!(String::from_utf8(content).unwrap()))
+                } else {
+                    Response::new(PROTOCOL, 404.to_string(), "Not Found")
+                }
             } else {
                 Response::new(PROTOCOL, 404.to_string(), "Not Found")
             };
